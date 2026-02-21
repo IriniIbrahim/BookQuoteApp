87d0e648d6c6b2c435ed7a14a9f25b029e411722
IriniIbrahim <iriniibrahim3@gmail.com>
2026-02-22 00:22:20 +0100

    adding edit form and change bg

--- BookQuoteUI/src/app/pages/books/books.ts
+++ BookQuoteUI/src/app/pages/books/books.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { Book } from '../../shared/models/book';
+import { NgClass, CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-books',
-  imports: [],
   templateUrl: './books.html',
-  styleUrl: './books.scss',
+  imports: [CommonModule, FormsModule],
 })
 export class Books {
+  books: Book[] = [
+    { id: 1, title: 'Clean Code', author: 'Robert C. Martin', year: 2008 },
+    { id: 2, title: 'Atomic Habits', author: 'James Clear', year: 2018 },
+  ];
+  deleteBook(id: number): void {
+    this.books = this.books.filter((b) => b.id !== id);
+  }
+  newTitle = '';
+  newAuthor = '';
+  newYear!: number;
 
+  addBook(): void {
+    if (!this.newTitle || !this.newAuthor || !this.newYear) return;
+
+    this.books.push({
+      id: Date.now(),
+      title: this.newTitle,
+      author: this.newAuthor,
+      year: this.newYear,
+    });
+
+    this.newTitle = '';
+    this.newAuthor = '';
+    this.newYear = 0;
+  }
 }
