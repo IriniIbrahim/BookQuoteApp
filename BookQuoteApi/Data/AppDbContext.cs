3ad7166aa343958d02470df90f444abc3f7e33d9
IriniIbrahim <iriniibrahim3@gmail.com>
2026-02-28 13:28:57 +0100

    update BookId for seeded quotes in AppDbContext

--- BookQuoteApi/Data/AppDbContext.cs
+++ BookQuoteApi/Data/AppDbContext.cs
@@ -19,11 +19,11 @@ public class AppDbContext : DbContext
             .IsUnique();
 
         modelBuilder.Entity<Quote>().HasData(
-            new Quote { Id = 1, Text = "The only way to do great work is to love what you do.", Author = "Steve Jobs", BookId = 0, IsArchived = false },
-            new Quote { Id = 2, Text = "Life is what happens when you're busy making other plans.", Author = "John Lennon", BookId = 0, IsArchived = false },
-            new Quote { Id = 3, Text = "The future belongs to those who believe in the beauty of their dreams.", Author = "Eleanor Roosevelt", BookId = 0, IsArchived = false },
-            new Quote { Id = 4, Text = "It is during our darkest moments that we must focus to see the light.", Author = "Aristotle", BookId = 0, IsArchived = false },
-            new Quote { Id = 5, Text = "In the end, we will remember not the words of our enemies, but the silence of our friends.", Author = "Martin Luther King Jr.", BookId = 0, IsArchived = false }
+            new Quote { Id = 1, Text = "The only way to do great work is to love what you do.", Author = "Steve Jobs", BookId = 1, IsArchived = false },
+            new Quote { Id = 2, Text = "Life is what happens when you're busy making other plans.", Author = "John Lennon", BookId = 1, IsArchived = false },
+            new Quote { Id = 3, Text = "The future belongs to those who believe in the beauty of their dreams.", Author = "Eleanor Roosevelt", BookId = 1, IsArchived = false },
+            new Quote { Id = 4, Text = "It is during our darkest moments that we must focus to see the light.", Author = "Aristotle", BookId = 1, IsArchived = false },
+            new Quote { Id = 5, Text = "In the end, we will remember not the words of our enemies, but the silence of our friends.", Author = "Martin Luther King Jr.", BookId = 1, IsArchived = false }
         );
     }
 }
